e69ffdaf1705b2ae1086e8fdf3ba811142006ccc
Yorbit <marco.95112@hotmail.it>
2022-06-21 16:37:57 +0200

    set over 65

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ console.log(userAge);
 const kmPrice = ('0.21');
 console.log(kmPrice);
 
+let ticketpriceDecimals = ticketFinalPrice.toFixed(2);
+console.log(ticketpriceDecimals, '€');
+
 let ticketFinalPrice = (kmNumb * kmPrice);
 
 
@@ -17,16 +20,28 @@ if (userAge <= 18) {
     const discountSmall = (ticketFinalPrice * 20) / 100;
 
     let discountSmallDecimals = (discountSmall.toFixed(2));
-
     console.log(discountSmallDecimals, '€');
 
     const priceDiscountSmall = (ticketFinalPrice - discountSmall);
 
     let priceDiscountSmallDecimals = (priceDiscountSmall.toFixed(2));
-
     console.log(priceDiscountSmallDecimals, '€');
 
 }
 
-let ticketDecimals = ticketFinalPrice.toFixed(2);
-console.log(ticketDecimals, '€');
+else if (userAge >= 65) {
+
+    const discountBig = (ticketFinalPrice * 40) / 100;
+
+    let discountBigDecimals = (discountBig.toFixed(2));
+    console.log(discountBigDecimals, '€');
+
+    const priceDiscountBig = (ticketFinalPrice - discountBig);
+
+    let priceDiscountBigDecimals = (priceDiscountBig.toFixed(2));
+    console.log(priceDiscountBigDecimals, '€');
+
+    let ticketDecimals = ticketFinalPrice.toFixed(2);
+    console.log(ticketDecimals, '€');
+}
+
