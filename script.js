2a4ba9086bc616706209eb4fd01627e1653183ed
Yorbit <marco.95112@hotmail.it>
2022-06-23 17:29:34 +0200

    fixed code

--- script.js
+++ script.js
@@ -2,19 +2,14 @@ console.log('JS OK');
 
 let messagePrice;
 
-const outputticketFinalPrice = document.getElementById('ticketFinalPrice');
-
-const kmNumb = parseInt(prompt('Inserisci qui quanti km devi percorrere'));
-console.log(kmNumb);
+const kmNumber = parseInt(prompt('Inserisci qui quanti km devi percorrere'));
+console.log(kmNumber);
 
 const Age = parseInt(prompt('Inserisci qui la tua età'));
 console.log(Age);
 
-const kmPrice = ('0.21');
-console.log(kmPrice);
-
-
-let ticketFinalPrice = (kmNumb * kmPrice);
+const ticketPrice = ('0.21');
+let ticketFinalPrice = (kmNumber * ticketPrice);
 
 let ticketpriceDecimals = ticketFinalPrice.toFixed(2);
 console.log(ticketpriceDecimals, '€');
@@ -22,38 +17,33 @@ console.log(ticketpriceDecimals, '€');
 if (Age < 18) {
 
     const discountSmall = (ticketFinalPrice * 20) / 100;
-
     let discountSmallDecimals = (discountSmall.toFixed(2));
     console.log(discountSmallDecimals, '€');
 
     const priceDiscountSmall = (ticketFinalPrice - discountSmall);
-
-    let priceDiscountSmallDecimals = (priceDiscountSmall.toFixed(2));
+    let priceDiscountSmallDecimals = (priceDiscountSmall.toFixed(2))
     console.log(priceDiscountSmallDecimals, '€');
 
-    document.getElementById('ticketFinalPrice').innerHTML = 'Il prezzo totale del biglietto' + ' ' + ticketFinalPrice - discountSmall + '€'
-
+    document.getElementById('ticketFinalPrice').innerHTML = 'Il prezzo totale del biglietto' + ' ' + priceDiscountSmallDecimals + '€';
 }
 
 else if (Age >= 65) {
 
     const discountBig = (ticketFinalPrice * 40) / 100;
-
     let discountBigDecimals = (discountBig.toFixed(2));
     console.log(discountBigDecimals, '€');
 
     const priceDiscountBig = (ticketFinalPrice - discountBig);
-
     let priceDiscountBigDecimals = (priceDiscountBig.toFixed(2));
     console.log(priceDiscountBigDecimals, '€');
 
-    document.getElementById('ticketFinalPrice').innerHTML = 'Il prezzo totale del biglietto' + ' ' + ticketFinalPrice - discountBig + '€'
-
+    document.getElementById('ticketFinalPrice').innerHTML = 'Il prezzo totale del biglietto' + ' ' + priceDiscountBigDecimals + '€';
 }
 
-let ticketDecimals = ticketFinalPrice.toFixed(2);
-console.log(ticketDecimals, '€');
+else {
 
-const paragraph = document.getElementById('ticketFinalPrice');
+    let ticketpriceDecimals = ticketFinalPrice.toFixed(2);
+    console.log(ticketpriceDecimals, '€');
 
-document.getElementById('ticketFinalPrice').innerHTML = 'Il prezzo totale del biglietto' + ' ' + ticketFinalPrice + ' ' + '€'
+    document.getElementById('ticketFinalPrice').innerHTML = 'Il prezzo totale del biglietto' + ' ' + ticketpriceDecimals + '€';
+}
